9bcd04ddc03c53083d7b5cd85c4bb1fa98eb382e
Chukwuyem Obuseh <89673167+charleside2001@users.noreply.github.com>
2021-11-10 16:24:48 -0600

    updated Pypoll.py file

--- Pypoll.py
+++ Pypoll.py
@@ -1,46 +1,3 @@
-# pseudocode - to create models or flowcharts for a programs
-# Indirect part
-import os
-dir(os)
-print(dir(os))
-dir(os.path)
-
-# Open the data file. data we need to retrieve - direct path
-# Assign a variable for the file to load and the path.
-file_to_load = os.path.join("Resources", "election_results.csv")
-# Open the election results and read the file
-with open(file_to_load) as election_data:
-    # Print the file object.
-     print(election_data)
-
-
-# Create a filename variable to a direct or indirect path to the file. - this creates a txt file and writes hello world to the txt file
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Use the open statement to open the file as a text file.
-outfile = open(file_to_save, "w")
-# Write some data to the file.
-outfile.write("Hello World")
-# Close the file
-outfile.close()
-
-
-# Create a filename variable to a direct or indirect path to the file. cleaner and more concise code
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-    # Write some data to the file.
-    #txt_file.write("Hello World")
-  # Write three counties to the file. - separate with commas to space the counties
-     #txt_file.write("Arapahoe, ")
-     #txt_file.write("Denver, ")
-    # txt_file.write("Jefferson")
-     # Use above or below codes to add counties
-    #txt_file.write("Arapahoe, Denver, Jefferson")
-     # Write three counties to the file.
-          txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
-
-
-# 3.4.4 Read the Election Results
 # Add our dependencies.
 import csv
 import os
@@ -48,52 +5,78 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Initialize a total vote counter.
+total_votes = 0
+
+# Candidate options and candidate votes
+candidate_options = []
+# 1. Declare the empty dictionary.
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-    # To do: read and analyze the data here.
-    # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
-        # Print each row in the CSV file.
-    #for row in file_reader:
-        #print(row)
-        # print(row[0]) to retrieve the first item from each row
-        # Read and print the header row.
+
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+
+            # 2. Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+# Print the candidate vote dictionary.
+print(candidate_votes)
 
 
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    
+    #  Modify the f-string print statement so the percentage is formatted to one decimal place       
+    # print(f"{candidate_name}: received {float(votes) / float(total_votes) * 100:.1f}% of the vote.") 
 
+    #  To do: print out each candidate's name, vote count, and percentage of
+    # votes to the terminal.
 
+    # Determine winning vote count and candidate
+    # Determine if the votes is greater than the winning count.   If true then set winning_count = votes and winning_percent =  vote_percentage.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # And, set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
 
-    # Write down the names of all the candidates - a complete list of  candidate
-    # Add a vote count for each candidate.- total number of vote cast
-    # Get the total votes for each candidate. - the percentage of votes each candidate won
-    # Get the total votes cast for the election. - total number of votes each candidate won
-    # The winner of the election based on the popular vote
-    # 
-    # 1. The  total number of vote cast
-    # 2. A complete list of  candidate 
-    # 3. The percentage of votes each candidate won
-    # 4. The total number of votes each candidate won
-    # 5. The winner of the election based on the popular vote
+#  To do: print out the winning candidate, vote count and percentage to terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-
-
-
-
-
-
-
-
-
-
-
-# Dependencies Dependencies are modules and packages, or a programming script that someone else has written, that allows you to increase the functional programming of your code, or speed and efficiency
-# Import the datetime class from the datetime module.
-#import datetime
-# Use the now() attribute on the datetime class to get the present time.
-#now = datetime.datetime.now()
-# Print the present time.
-#print("The time right now is ", now)
-
-# Dependencies - Modules
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
